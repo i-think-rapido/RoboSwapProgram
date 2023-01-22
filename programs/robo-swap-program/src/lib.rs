f9c8fdd815822b70f80f821e4f52da483f40a6fc
Romeo Disca <romeo.disca@gmail.com>
2023-01-22 21:38:24 +0100

    created anchor program

--- programs/robo-swap-program/src/lib.rs
+++ programs/robo-swap-program/src/lib.rs
@@ -1,3 +1,4 @@
+
 use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -6,27 +7,97 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod robo_swap_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Game>, wallet: Pubkey) -> Result<()> {
-        for idx in 0..game::ROBOTS {
-            let robot = &mut ctx.accounts.game.robots[idx];
-            *robot = game::Robot::new(wallet, idx as u8);
-        }
-        ctx.accounts.game.bump = *ctx.bumps.get("game").unwrap_or_else(|| panic!("hundt"));
+    pub fn delete(
+        _ctx: Context<Delete>
+    ) -> Result<()> {
         Ok(())
     }
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        for idx in 0..game::ROBOTS {
+            let robot = &mut ctx.accounts.pda.robots[idx];
+            *robot = game::Robot::new(ctx.accounts.user.key(), idx as u8)?;
+        }
+        ctx.accounts.pda.bump = *ctx.bumps.get("pda").unwrap_or_else(|| panic!("hundt"));
+        Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>, victim_idx: u8, robber_idx: u8) -> Result<()> {
+        
+        require!(robber_idx <= 25, game::RoboSwapError::IndexOutOfBounds);
+        require!(victim_idx <= 25, game::RoboSwapError::IndexOutOfBounds);
+
+        let r = &mut ctx.accounts.robber_pda.robots[robber_idx as usize];
+        let v = &mut ctx.accounts.victim_pda.robots[victim_idx as usize];
+
+        let helper = r.owner;
+        r.owner = v.owner;
+        v.owner = helper;
+
+        let helper = r.owner_idx;
+        r.owner_idx = v.owner_idx;
+        v.owner_idx = helper;
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
-pub struct Game<'info> {
+pub struct Delete<'info> {
+    pub system_program: Program<'info, System>,
+    
+    /// CHECK: This is not dangerous
+    #[account(
+        mut,
+        seeds = [b"RoboSwap", receiver.key().as_ref()], bump,
+        close = receiver,
+    )]
+    pub pda: Box<Account<'info, game::Game>>,
+
+    #[account(
+        mut,
+    )]
+    pub receiver: SystemAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    pub system_program: Program<'info, System>,
+    
     #[account(mut)]
     pub user: Signer<'info>,
+
     #[account(
         init, payer = user, space = 8 + game::GAME_SIZE, 
         seeds = [b"RoboSwap", user.key().as_ref()], bump,
-    )]
-    pub game: Box<Account<'info, game::Game>>,
-    pub system_program: Program<'info, System>,
+    )]  
+    pub pda: Box<Account<'info, game::Game>>,
 }
+
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    pub system_program: Program<'info, System>,
+
+    #[account(mut)]
+    pub robber: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"RoboSwap", robber.key().as_ref()], bump,
+    )]  
+    pub robber_pda: Box<Account<'info, game::Game>>,
+
+    #[account(mut)]
+    pub victim: SystemAccount<'info>,
+    #[account(
+        mut,
+        seeds = [b"RoboSwap", victim.key().as_ref()], bump,
+    )]  
+    pub victim_pda: Box<Account<'info, game::Game>>,
+}
+
+#[account]
+pub struct NewAccount(u8);
 
 mod game {
     use anchor_lang::prelude::*;
@@ -44,23 +115,31 @@ mod game {
     impl Game {}
 
     #[derive(AnchorSerialize, AnchorDeserialize, Default, Clone, Copy, PartialEq, Eq)]
-    pub struct Robot {
-        wallet: Pubkey,
-        owner: Pubkey,
-        idx: u8,
-        owner_idx: u8,
-        swaps: u32,
+    pub     struct Robot {
+        pub wallet: Pubkey,
+        pub owner: Pubkey,
+        pub idx: u8,
+        pub owner_idx: u8,
+        pub swaps: u32,
     }
     impl Robot {
-        pub fn new(wallet: Pubkey, idx: u8) -> Self {
-            Self {
+        pub fn new(wallet: Pubkey, idx: u8) -> Result<Self> {
+            require!(idx <= 25, RoboSwapError::IndexOutOfBounds);
+            Ok(Self {
                 owner: wallet.clone(),
                 wallet,
                 idx,
                 owner_idx: idx,
                 swaps: 0,
-            }
+            })
         }
     }
+
+    #[error_code]
+    pub enum RoboSwapError {
+        IndexOutOfBounds,
+        UserAndAccountNotEqual,
+    }
+
 
 }
--- tests/robo-swap-program.ts
+++ tests/robo-swap-program.ts
@@ -1,48 +1,152 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { PublicKey } from "@solana/web3.js";
-import { expect } from "chai";
+import { PublicKey, Keypair } from "@solana/web3.js";
+import { assert, expect } from "chai";
 import { RoboSwapProgram } from "../target/types/robo_swap_program";
 import _ from "lodash";
+import winston from "winston";
+
+const winstonFormat = winston.format.printf((info) => 
+  `${new Date().toISOString()}-${info.level}: ${JSON.stringify(info.message, null, 4)}`
+)
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File({ filename: '../log.log', format: winstonFormat })
+  ],
+})
 
 describe("robo-swap-program", () => {
+  
   const provider = anchor.AnchorProvider.env()
   anchor.setProvider(provider);
-
   const program = anchor.workspace.RoboSwapProgram as Program<RoboSwapProgram>;
 
-  it("Is initialized!", async () => {
+  let robber: PublicKey
+  let robberPDA: PublicKey
 
-    const [gamePDA, _bump] = PublicKey
-      .findProgramAddressSync([
-          anchor.utils.bytes.utf8.encode("RoboSwap"),
-          provider.wallet.publicKey.toBuffer(),
-        ],
-        program.programId,
-      )
+  before(async () => {
+    robber = provider.wallet.publicKey
+    await airdrop(robber, provider)
+    robberPDA = await initialize(robber, program)
+
+    logger.info('----------------------------------------')
+  })
+  after(async() => {
+    await program.methods
+      .delete()
+      .accounts({
+        receiver: robber,
+      })
+  })
+
+    it("Is initialized!", async () => {
+        const robots = (await program.account.game.fetch(robberPDA)).robots;
+      _.range(26).map(d => {
+        const robot = robots[d]
+        expect(robot.wallet.toBase58()).to.equal(robber.toBase58())
+        expect(robot.owner.toBase58()).to.equal(robber.toBase58())
+        expect(robot.idx).to.equal(d)
+        expect(robot.ownerIdx).to.equal(d)
+        expect(robot.swaps).to.equal(0)
+      })
+    });
+  
+    it("Swap!", async () => {
+  
+      const keypair = Keypair.generate()
+      await createAccount(keypair, provider)
+      const victim = keypair.publicKey
+      const victimPDA = await initialize(victim, program, keypair)
+  
+      const robberIdx = 3;
+      const victimIdx = 8;
+  
+      try {
+        await program.methods
+          .swap(victimIdx, robberIdx)
+          .accounts({
+            robber,
+            robberPda: robberPDA,
+            victim,
+            victimPda: victimPDA,
+          })
+          .rpc();
+      }
+      catch (err) {
+        console.error(err)
+        //assert(false)
+      }
+
+      let r = (await program.account.game.fetch(robberPDA)).robots[robberIdx]
+      let v = (await program.account.game.fetch(victimPDA)).robots[victimIdx]
+      expect(v.ownerIdx).to.equal(robberIdx)
+      expect(r.ownerIdx).to.equal(victimIdx)
+      expect(v.owner.toBase58()).to.equal(robber.toBase58())
+      expect(r.owner.toBase58()).to.equal(victim.toBase58())
+  
+      await program.methods.delete().accounts({receiver: victim, pda: victimPDA}).rpc()
+    });
+  
+});
+
+const initialize = async (user: PublicKey, program: any, keypair?: Keypair|undefined): Promise<PublicKey> => {
+  const [pda, _] = PublicKey
+    .findProgramAddressSync([
+        anchor.utils.bytes.utf8.encode("RoboSwap"),
+        user.toBuffer(),
+      ],
+      program.programId,
+    )
 
     try {
 
-    const tx = await program.methods
-      .initialize(provider.wallet.publicKey)
+    let tx = program.methods
+      .initialize()
       .accounts({
-        user: provider.wallet.publicKey,
-        game: gamePDA,
+        user: user,
+        pda,
       })
-      .rpc();
+      if (keypair) {
+        tx.signers([keypair])
+      }
+      await tx.rpc();
     }
-    catch (err) {
-      console.error(err)
-    }
+  catch (err) {
+    logger.error(err)
+    assert(false, err)
+  }
+  return pda
+}
 
-    const robots = (await program.account.game.fetch(gamePDA)).robots;
-    _.range(26).map(d => {
-      const robot = robots[d]
-      expect(robot.wallet.toBase58()).to.equal(provider.wallet.publicKey.toBase58())
-      expect(robot.owner.toBase58()).to.equal(provider.wallet.publicKey.toBase58())
-      expect(robot.idx).to.equal(d)
-      expect(robot.ownerIdx).to.equal(d)
-      expect(robot.swaps).to.equal(0)
+const airdrop = async (pubkey: PublicKey, provider: anchor.AnchorProvider) => {
+  try {
+    const signature = await provider.connection.requestAirdrop(
+      pubkey, 10000000000
+    )
+  }
+  catch (err) {
+    assert(false, err)
+  }
+}
+
+const createAccount = async (keypair: Keypair, provider: anchor.AnchorProvider) => {
+  try {
+    const instruction = anchor.web3.SystemProgram.createAccount({
+      fromPubkey: provider.wallet.publicKey,
+      newAccountPubkey: keypair.publicKey,
+      space: 0,
+      lamports: 100000000,
+      programId: anchor.web3.SystemProgram.programId,
     })
-  });
-});
+    let tx = new anchor.web3.Transaction().add(instruction)
+    const latestBlockhash = await provider.connection.getLatestBlockhash()
+    tx.recentBlockhash = latestBlockhash.blockhash
+    tx.feePayer = provider.wallet.publicKey
+    tx = await new anchor.Wallet(keypair).signTransaction(tx)
+    const signature = await provider.sendAndConfirm(tx)
+  }
+  catch (err) {
+    assert(false)
+  }
+  
+}

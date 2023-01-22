3fa367704ef0687e7614a0cb0db87d83ba70eae7
Romeo Disca <romeo.disca@gmail.com>
2023-01-22 20:33:46 +0100

    initialization of PDA has been fullfilled

--- programs/robo-swap-program/src/lib.rs
+++ programs/robo-swap-program/src/lib.rs
@@ -6,10 +6,61 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod robo_swap_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Game>, wallet: Pubkey) -> Result<()> {
+        for idx in 0..game::ROBOTS {
+            let robot = &mut ctx.accounts.game.robots[idx];
+            *robot = game::Robot::new(wallet, idx as u8);
+        }
+        ctx.accounts.game.bump = *ctx.bumps.get("game").unwrap_or_else(|| panic!("hundt"));
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Game<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init, payer = user, space = 8 + game::GAME_SIZE, 
+        seeds = [b"RoboSwap", user.key().as_ref()], bump,
+    )]
+    pub game: Box<Account<'info, game::Game>>,
+    pub system_program: Program<'info, System>,
+}
+
+mod game {
+    use anchor_lang::prelude::*;
+    use anchor_lang::{prelude::Pubkey, account};
+
+    pub const ROBOTS: usize = 26;
+    pub const ROBOT_SIZE: usize = 70;
+    pub const GAME_SIZE: usize = ROBOT_SIZE * ROBOTS + 1;
+
+    #[account]
+    pub struct Game {
+        pub robots: [Robot; ROBOTS],
+        pub bump: u8,
+    }
+    impl Game {}
+
+    #[derive(AnchorSerialize, AnchorDeserialize, Default, Clone, Copy, PartialEq, Eq)]
+    pub struct Robot {
+        wallet: Pubkey,
+        owner: Pubkey,
+        idx: u8,
+        owner_idx: u8,
+        swaps: u32,
+    }
+    impl Robot {
+        pub fn new(wallet: Pubkey, idx: u8) -> Self {
+            Self {
+                owner: wallet.clone(),
+                wallet,
+                idx,
+                owner_idx: idx,
+                swaps: 0,
+            }
+        }
+    }
+
+}
--- tests/robo-swap-program.ts
+++ tests/robo-swap-program.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { PublicKey } from "@solana/web3.js";
+import { expect } from "chai";
 import { RoboSwapProgram } from "../target/types/robo_swap_program";
+import _ from "lodash";
 
 describe("robo-swap-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.RoboSwapProgram as Program<RoboSwapProgram>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+    const [gamePDA, _bump] = PublicKey
+      .findProgramAddressSync([
+          anchor.utils.bytes.utf8.encode("RoboSwap"),
+          provider.wallet.publicKey.toBuffer(),
+        ],
+        program.programId,
+      )
+
+    try {
+
+    const tx = await program.methods
+      .initialize(provider.wallet.publicKey)
+      .accounts({
+        user: provider.wallet.publicKey,
+        game: gamePDA,
+      })
+      .rpc();
+    }
+    catch (err) {
+      console.error(err)
+    }
+
+    const robots = (await program.account.game.fetch(gamePDA)).robots;
+    _.range(26).map(d => {
+      const robot = robots[d]
+      expect(robot.wallet.toBase58()).to.equal(provider.wallet.publicKey.toBase58())
+      expect(robot.owner.toBase58()).to.equal(provider.wallet.publicKey.toBase58())
+      expect(robot.idx).to.equal(d)
+      expect(robot.ownerIdx).to.equal(d)
+      expect(robot.swaps).to.equal(0)
+    })
   });
 });
